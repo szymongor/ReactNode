b35d491a9b74e9f559425140e6aaca68c6af7b12
szymongor <gornioczek.szymon@gmail.com>
2018-02-12 12:09:33 +0100

    User model

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys.js');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -11,9 +14,15 @@ passport.use(
       proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('AccessToken: ', accessToken);
-      console.log('refreshToken: ', refreshToken);
-      console.log('profile: ', profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );

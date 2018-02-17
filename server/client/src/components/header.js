cc3cb0e2e493249c5083a1ba60c7ed30f986eac1
szymongor <gornioczek.szymon@gmail.com>
2018-02-17 13:47:51 +0100

    Redirect on auth

--- server/client/src/components/header.js
+++ server/client/src/components/header.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">ReactNode</a>
-          <ul className="right hide-on--med-and-down">
-            <li>
-              <a>Login With Google</a>
-            </li>
+          <ul className="right">
+            <li>{this.renderContent()}</li>
           </ul>
         </div>
       </nav>
@@ -17,4 +35,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,6 +1,9 @@
+import { FETCH_USER } from '../actions/types';
+
 export default function(state = {}, action) {
-  console.log(('authReducer', action));
   switch (action.type) {
+    case FETCH_USER:
+      return action.palyoad || false;
     default:
       return state;
   }

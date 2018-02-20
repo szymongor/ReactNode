9c5222d9a82e6c5a2121d26be657f7696a41ae57
szymongor <gornioczek.szymon@gmail.com>
2018-02-21 00:02:57 +0100

    Header and Landing

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -18,7 +18,8 @@ module.exports = app => {
 
   app.get('/api/logout', (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect('/');
+    //res.send(req.user);
   });
 
   app.get('/api/current_user', (req, res) => {

b55f815b4ba1f98df97588b9f1913c9a2bf659c0
szymongor <gornioczek.szymon@gmail.com>
2018-02-22 00:19:27 +0100

    Payments via Stripe

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -16,3 +16,6 @@ ReactDOM.render(
   </Provider>,
   document.querySelector('#root')
 );
+
+console.log('Stripe key is: ', process.env.REACT_APP_STRIPE_KEY);
+console.log('Env is: ', process.env.NODE_ENV);
